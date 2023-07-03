6b1aecf15e93751fa6203c955b76e583bb217c62
Sebastián Illa <sebailla@sebastians-mbp.www.duckdns.org>
2023-07-03 17:52:57 -0300

    primer comit preentrega 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import './App.css';
-import Navbar from './components/navbar/Navbar';
-import ItemContainer from './containers/ItemContainer/ItemContainer';
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import CartContainer from './containers/cart';
 
 
 function App() {
   return (
-
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path={'/'} element={<ItemContainer greeting="Esta es un Nuevo proyecto de React"></ItemContainer>} />
-        <Route path={'/product/:category'} element={<ItemContainer/>} />
-        <Route path={'/product/:id'} element={<p>Producto 1</p> } />
-        <Route path={'/cart'} element={<CartContainer/>} />
-      </Routes>
-    </BrowserRouter>
-
+    <>
+    <h1>Hola Mundo !</h1>
+    </>
   );
 }
 
--- src/sdk/products.js
+++
@@ -1,3 +0,0 @@
-export const getProducts = (search) => {
-    return fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${search}`); 
-}
