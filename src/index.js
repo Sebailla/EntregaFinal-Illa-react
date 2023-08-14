6107aad4d331121e7c492ef880979abfd5229bce
Sebastián Illa <sebailla@Sebastians-MacBook-Pro.local>
2023-08-14 19:41:35 -0300

    commit final-1

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 
-const firebaseConfig = {
+/* const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY || '',
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || '',
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || '',
@@ -15,6 +15,16 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || '',
   appId: process.env.REACT_APP_FIREBASE_APP_ID || '',
   measurementId: process.env.REACT_APP_FIREBASE_MEASURE_ID || ''
+}; */
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmDulBJoIf0_WNlTaHKongPcAqeLfdK40",
+  authDomain: "aqualife-e93c1.firebaseapp.com",
+  projectId: "aqualife-e93c1",
+  storageBucket: "aqualife-e93c1.appspot.com",
+  messagingSenderId: "873380815817",
+  appId: "1:873380815817:web:d8ad976b97c0df1f27be18",
+  measurementId: "G-W1NT136FYW"
 };
 
 // Initialize Firebase
